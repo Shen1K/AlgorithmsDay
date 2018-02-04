7e5feefd01646bf0fe8650158c080af7eae03685
Shen1K <2997735722@qq.com>
2018-02-04 20:03:26 +0800

    Create Stack and Queue by ChainedList
    Change-Id: I06e2c2801086d9a4c78ddc8efa53d2bc935ef840
    Signed-off-by: Shen1K <2997735722@qq.com>

---
+++ src/StaticSETofInts/Whitelist.java
@@ -0,0 +1,11 @@
+package StaticSETofInts;
+
+
+public class Whitelist {
+	public static void main(String[] args){
+		int[] w = {11,14,23,15,26,12,8,19,31};
+		StaticSetofInts set = new StaticSetofInts(w);
+		int key = 11;
+		System.out.println(set.contains(key));
+	}
+}
